aeee35a4040df1c4913a9f093d5529ff445f692c
bogdan228Q <145839576+bogdan228Q@users.noreply.github.com>
2023-09-23 13:19:53 +0300

    Create gene.py

---
+++ gene.py
@@ -0,0 +1,9 @@
+print('здравствуйте это викторина выйграй милион!!!!!')
+import random
+number = random.randint(1, 1000000)
+import time
+print('это число...')
+time.sleep(4)
+print (number)
+time.sleep(4)
+print('ты получил деньги')
